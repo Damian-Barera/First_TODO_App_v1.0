1256cad1f2b0cd734e3dbe9a3a91ea144cb1271e
Damian Barera <team@platzi.com>
2022-12-07 13:49:11 -0300

    Updates

---
+++ precache-manifest.40ba197c1bf9ad33667f55b1da796325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31811c9f5ac90f0c1dc4",
+    "url": "/First_TODO_App_v1.0/static/css/main.0490b287.chunk.css"
+  },
+  {
+    "revision": "31811c9f5ac90f0c1dc4",
+    "url": "/First_TODO_App_v1.0/static/js/main.1ccc7fae.chunk.js"
+  },
+  {
+    "revision": "41af6e2626800ba6dfa3",
+    "url": "/First_TODO_App_v1.0/static/js/runtime~main.7d1abbba.js"
+  },
+  {
+    "revision": "107ed4f0a3fd52f1ebcb",
+    "url": "/First_TODO_App_v1.0/static/js/2.759c6e37.chunk.js"
+  },
+  {
+    "revision": "a6ff2ce648ff3421426db7842d40d1d0",
+    "url": "/First_TODO_App_v1.0/static/media/check.a6ff2ce6.svg"
+  },
+  {
+    "revision": "45d0d71a1f8f92c1da6744303a373163",
+    "url": "/First_TODO_App_v1.0/static/media/delete.45d0d71a.svg"
+  },
+  {
+    "revision": "c023e133448533c0cc7127012b24f22c",
+    "url": "/First_TODO_App_v1.0/index.html"
+  }
+];
